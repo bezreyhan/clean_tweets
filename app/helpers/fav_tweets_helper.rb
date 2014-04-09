21e51fff412a27bb3e49ae8d5a2b408347788253
bezreyhan <bezreyhan@gmail.com>
2014-04-08 22:26:37 -0700

    make users/new mobile friendly

--- app/helpers/fav_tweets_helper.rb
+++ app/helpers/fav_tweets_helper.rb
@@ -10,37 +10,31 @@ module FavTweetsHelper
       # find index of white space or use the length of the text
       last = new_text.index(/\s/, start) || new_text.length
       link = new_text.slice!(start..last)
-      # new_text = new_text.insert(start, "#{link_to link, tweet.attrs[:entities][:urls].first[:expanded_url] }" )
-      if tweet.attrs[:entities][:media] == nil
-        new_text = new_text.insert(start, "#{link_to link, tweet.attrs[:entities][:urls][iterator-1][:expanded_url] }" )
+      # if the tweet has pics and we are iterating over the last
+      # link, which is always the pic, then we should link to the urls
+      # for the image of the pic, else, link to the appropriate url. 
+      if tweet.attrs[:entities][:media] != nil && num_links == iterator
+        new_text = new_text.insert(start, "#{link_to link, tweet.attrs[:entities][:media].first[:media_url_https] }" )
       else
-        if num_links == iterator 
-          new_text = new_text.insert(start, "#{link_to link, tweet.attrs[:entities][:media].first[:media_url_https] }" )
-        else
-          new_text = new_text.insert(start, "#{link_to link, tweet.attrs[:entities][:urls][iterator-1][:expanded_url] }" )
-        end
+        new_text = new_text.insert(start, "#{link_to link, tweet.attrs[:entities][:urls][iterator-1][:expanded_url] }" )
       end
+
+
+      # if tweet.attrs[:entities][:media] == nil
+      #   new_text = new_text.insert(start, "#{link_to link, tweet.attrs[:entities][:urls][iterator-1][:expanded_url] }" )
+      # else
+      #   if num_links == iterator 
+      #     new_text = new_text.insert(start, "#{link_to link, tweet.attrs[:entities][:media].first[:media_url_https] }" )
+      #   else
+      #     new_text = new_text.insert(start, "#{link_to link, tweet.attrs[:entities][:urls][iterator-1][:expanded_url] }" )
+      #   end
+      # end
 
       end_of_link = new_text.index("</a>", idx)
       idx = end_of_link + 4
       iterator += 1
-      # if num_links >= 3
-      #   binding.pry
-      # end
     end
     return new_text
   end
-
-  # def insert_link new_text start tweet link iterator num_links
-  #   if tweet.attrs[:entities][:media] == nil
-  #     new_text = new_text.insert(start, "#{link_to link, tweet.attrs[:entities][:urls][iterator-1][:expanded_url] }" )
-  #   else
-  #     if num_links == iterator 
-  #       new_text = new_text.insert(start, "#{link_to link, tweet.attrs[:entities][:media].first[:media_url_https] }" )
-  #     else
-  #       new_text = new_text.insert(start, "#{link_to link, tweet.attrs[:entities][:urls][iterator-1][:expanded_url] }" )
-  #     end
-  #   end
-  # end
 
 end
